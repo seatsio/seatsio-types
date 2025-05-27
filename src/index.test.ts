65495bd39f57258286e9204980f30b52f4ccc93c
mroloux <matti@seats.io>
2025-05-27 10:05:49 +0200

    Edit order mode (#40)
    * Added editOrder mode
    
    * Added listOrderChanges
    
    * Removed unused interface

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,9 @@
 import {
     ChartDesignerConfigOptions,
     ChartRendererConfigOptions,
-    EventManagerConfigOptions, EventManagerCreateOrderModeConfigOptions,
+    EventManagerConfigOptions,
+    EventManagerCreateOrderModeConfigOptions,
+    EventManagerEditOrderModeConfigOptions,
     EventManagerFilterSectionsModeConfigOptions,
     EventManagerManageCategoriesModeConfigOptions,
     EventManagerManageChannelsModeConfigOptions,
@@ -399,6 +401,43 @@ const eventManagerCreateOrderModeConfig: Required<EventManagerCreateOrderModeCon
     onFilteredCategoriesChanged: _categories => {}
 }
 
+// Edit order mode
+const eventManagerEditOrderModeConfig: Required<EventManagerEditOrderModeConfigOptions> = {
+    ...fullEventManagerConfig,
+    mode: 'createOrder',
+    events: ['eventA', 'eventB'],
+    pricing: [
+        { category: 'A', price: 10, originalPrice: 15 }
+    ],
+    priceFormatter: price => '$' + price,
+    selectionValidators: [{ type: 'noOrphanSeats'}],
+    maxSelectedObjects: 4,
+    objectWithoutPricingSelectable: true,
+    categoryFilter: { enabled: true },
+    availableCategories: [1, 2, 'Balcony'],
+    unavailableCategories: [3, 4, 'Stalls'],
+    filteredCategories: ['Stalls'],
+    channels: ['0ef73fd9-693c-5073-98ac-d1dd8cd86536', 'NO_CHANNEL'],
+    session: 'continue',
+    holdToken: 'myToken',
+    legend: {},
+    showLegend: true,
+    order: [{label: 'A-1'}, 'A-2', {label: 'A-3', ticketType: 'adult', amount: 1}],
+
+    // Callbacks.
+    onSessionInitialized: _holdToken => { },
+    onHoldSucceeded: (_objects, _ticketTypes) => {},
+    onHoldFailed: (_objects, _ticketTypes) => {},
+    onHoldTokenExpired: () => {},
+    onReleaseHoldSucceeded: (_objects, _ticketTypes) => {},
+    onReleaseHoldFailed: (_objects, _ticketTypes) => {},
+    onSelectionValid: () => {},
+    onSelectionInvalid: () => {},
+    onHoldCallsInProgress: () => {},
+    onHoldCallsComplete: () => {},
+    onFilteredCategoriesChanged: _categories => {}
+}
+
 // Set up a complete Chart Designer config - normal mode
 const chartDesignerConfigNormalMode: Required<ChartDesignerConfigOptions> = {
     secretKey: 'mySecretKey',
@@ -502,11 +541,11 @@ const seatingChart = new seatsio.SeatingChart({
     event: 'myEvent'
 })
 
-new seatsio.EventManager({
+let eventManager = new seatsio.EventManager({
     event: 'myEvent',
     secretKey: 'mySecretKey',
     mode: 'static'
-})
+});
 
 new seatsio.SeatingChartDesigner({
     secretKey: 'mySecretKey'
@@ -535,4 +574,9 @@ seatingChart.listSelectedObjects().then(objects => {
         obj.selected
         obj.selectedTicketType
     })
+})
+
+// Event Manager tests
+eventManager.listOrderChanges().forEach(orderChange => {
+    console.log(orderChange.type, orderChange.object, orderChange.ticketType)
 })
