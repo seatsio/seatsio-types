18ed70289be70465c91696c9f4d27ca570e95361
Morten Devold <morten@seats.io>
2025-06-06 11:49:59 +0200

    Update pricing to include new object format

--- src/index.ts
+++ src/index.ts
@@ -1290,7 +1290,12 @@ export type PricingForCategory = (SimplePricing | MultiLevelPricing)
 
 export type PricingForObjects = (SimplePricingForObjects | MultiLevelPricingForObjects)
 
-export type Pricing = (PricingForCategory | PricingForObjects)[]
+export type Pricing = ObjectPricing | LegacyPricing
+export type LegacyPricing = (PricingForCategory | PricingForObjects)[]
+export type ObjectPricing = {
+    priceFormatter?: (price: number) => string
+    prices: (PricingForCategory | PricingForObjects)[]
+}
 
 export type SelectionValidator =
     SelectionValidatorNoOrphanSeats
